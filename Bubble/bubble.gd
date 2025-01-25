5f53e8cb609816c329a6547d4b502dd34bbd076b
XaSpartanGhost <xabv50@gmail.com>
2025-01-25 02:29:17 +0100

    bubbles part 1

---
+++ Bubble/bubble.gd
@@ -0,0 +1,31 @@
+class_name Bubble
+extends Node2D
+
+@export var direction : Vector2 = Vector2(0,0)
+var bubbleT: GameController.bubbleType = GameController.bubbleType.blue
+# Numero de rebotes que puede resistir
+var endurance: float = 0
+
+#func _init(dir, type):
+	#direction = dir
+	#bubbleT = type
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+		$RigidBody2D.move_and_collide(direction)
+
+
+func onCollisionEnter(body: Node) -> void:
+	if body.is_in_group("Bubbles"):
+		endurance += (body.get_parent() as Bubble).endurance
+		direction += (body.get_parent() as Bubble).direction
+		bubbleT = GameController.bubbleType.purple
+		body.get_parent().free()
+		
+		
