7debf03997346d29f5a8adc3f4d34a45bfb2ca4a
Manuel Otero <ManuelOtero.af@gmail.com>
2025-01-26 00:01:27 +0100

    pause

--- GameController.gd
+++ GameController.gd
@@ -9,3 +9,10 @@ enum bubbleType {
 var bubbleColor = {bubbleType.blue: Color.CORNFLOWER_BLUE, bubbleType.purple: Color.PURPLE}
 
 var last_checkpoint_position = Vector2(0,0)
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _input(event: InputEvent) -> void: 
+	if event.is_action_pressed("pause",true):
+		get_tree().paused = not get_tree().paused
