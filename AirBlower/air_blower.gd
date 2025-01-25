c73614559ed3b3bfb347d58aab2d189ab2ac4585
XaSpartanGhost <xabv50@gmail.com>
2025-01-25 15:23:27 +0100

    Ajustes tamaño y airblowerv1

---
+++ AirBlower/air_blower.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var impulse : Vector2;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") or body.is_in_group("Bubble"):
+		body.impul
