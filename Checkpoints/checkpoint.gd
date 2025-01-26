6466797321403cdf477b0a399ca53568299fba8a
XaSpartanGhost <xabv50@gmail.com>
2025-01-26 15:09:08 +0100

    el final ahora si esta vez de verdad

--- Checkpoints/checkpoint.gd
+++ Checkpoints/checkpoint.gd
@@ -8,5 +8,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and not actived:
 		actived = true
 		$AudioStreamPlayer2D.play()
-		GameController.last_checkpoint_position = position
+		GameController.last_checkpoint_position = global_position
 		$CollisionShape2D/Sprite2D.texture = textureInactive
