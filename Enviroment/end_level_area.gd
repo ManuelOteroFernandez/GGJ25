9b149f521a1c884611deafd994838253962e141e
Manuel Otero <manuelOtero.af@gmail.com>
2026-04-15 10:58:45 +0200

    End level area customizable

--- Enviroment/end_level_area.gd
+++ Enviroment/end_level_area.gd
@@ -1,5 +1,14 @@
+@tool
 extends Area2D
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+@export var shape: RectangleShape2D:
+	set(value):
+		shape = value
+		if not is_node_ready():
+			return
+		collision_shape_2d.shape = shape
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
